f023a4d8eb66ca4e3097b729e852b5c410a6007e
ansehyeong <dkstpgud@gmail.com>
2023-01-15 00:04:51 +0900

    feat: reflection field 타입으로 클래스 혹은 인스턴스의 필드와 할당된 값을 가지고 올 수 있다.

--- src/main/java/anpopo/reflect_fields/FieldsMain.java
+++ src/main/java/anpopo/reflect_fields/FieldsMain.java
@@ -4,24 +4,29 @@ import java.lang.reflect.Field;
 
 public class FieldsMain {
 
-    public static void main(String[] args) throws NoSuchFieldException {
-//        Movie avatar = new Movie("아바타 물의길", 2023, true, Category.ADVENTURE, 20.99);
-//        FieldsMain.printFields(avatar.getClass());
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
+        Movie avatar = new Movie("아바타 물의길", 2023, true, Category.ADVENTURE, 20.99);
+        FieldsMain.printFields(avatar.getClass(), avatar);
 
-        printFields(Movie.class);
         System.out.println("------------- Line for check synthetic field");
-        printFields(Category.class);
+        printFields(Category.class, null);
     }
 
-    public static <T> void printFields(Class<T> clazz) {
+    public static <T> void printFields(Class<? extends T> clazz, T instance) throws IllegalAccessException {
+
         StringBuilder sb = new StringBuilder();
-        if (clazz != null) {
-            printFields(clazz.getSuperclass());
+        if (clazz != null && !clazz.getSuperclass().getSimpleName().equals("Object")) {
+            printFields(clazz.getSuperclass(), instance);
 
             for (Field field : clazz.getDeclaredFields()) {
                 sb.append(String.format("field name : %s, type : %s\n", field.getName(), field.getType().getName()))
-                    .append(String.format("is synthetic : %s", field.isSynthetic()))
-                    .append("\n");
+                    .append(String.format("is synthetic : %s\n", field.isSynthetic()));
+
+                if (!field.isEnumConstant()) {
+                    sb.append(String.format("value : %s", field.get(instance)));
+                }
+
+                sb.append("\n");
             }
 
             System.out.println(sb);
